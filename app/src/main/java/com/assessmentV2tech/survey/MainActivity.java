015b2663c9d5064fb905beab0631b5a30cd93adc
Syed Parvez Antor <36919896+antorparvez@users.noreply.github.com>
2020-07-29 00:15:39 +0600

    button activity added ( activity_submit_survey.xml activity_view_survey.xml)

--- app/src/main/java/com/assessmentV2tech/survey/MainActivity.java
+++ app/src/main/java/com/assessmentV2tech/survey/MainActivity.java
@@ -2,13 +2,51 @@ package com.assessmentV2tech.survey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button viewSurveyBtn,submitSurveyBtn;
+    private ImageView main_logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //View survey button
+        viewSurveyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,ViewSurveyActivity.class));
+                finish();
+
+
+            }
+        });   //Submit survey button
+        submitSurveyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,SubmitSurveyActivity.class));
+                finish();
+            }
+        });
+
+
+
+
+    }
+
+
+    private void initView() {
+        viewSurveyBtn = findViewById(R.id.viewSurveyBtn);
+        submitSurveyBtn = findViewById(R.id.surveyBtn);
+        main_logo = findViewById(R.id.main_logo);
+
+
     }
 }
